dc3461a0706633f77482b6eadfa0ee7869308611
Roscoe <elysiaroscoe@gmail.com>
2021-10-18 18:27:45 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var video = document.getElementById("video")
+
+function over(element) {
+    play()  
+}
+    
+function out(element) {
+    pause() 
+}
+
+
+
+function beginPlay(element){
+    console.log(element);
+
+}
+
+function endPlay(element){
+
+}
